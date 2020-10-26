75f0adc25c072c763bb27b55ec8e663badf653bd
Kirill Ivanov <puaktopblm@yandex.ru>
2020-10-26 13:47:50 +0700

    посылает команду capture, принимает результат - если это картинка, то выводит ее. В конце посылает команду kill

--- client.py
+++ client.py
@@ -1,4 +1,8 @@
 import zmq
+import json
+import json_coder.py.json_coder as json_coder
+from json_coder.py.messagetypes import *
+import cv2
 
 context = zmq.Context()
 #  Socket to talk to server
@@ -7,7 +11,20 @@ socket = context.socket(zmq.REQ)
 socket.connect("tcp://localhost:5554")
 
 
-socket.send(bytes("kill", 'utf-8'))
-print("message send")
-message = socket.recv().decode('utf-8')
-print("recieve message: {0}".format(message))
+
+kill_task = ServiceTask("capture")
+j_str = json.dumps(kill_task, cls=json_coder.coder.CustomEncoder)
+socket.send(bytes(j_str, 'utf-8'))
+capture_answer_str = socket.recv().decode('utf-8')
+j_cap_answ = json.loads(capture_answer_str, object_hook=json_coder.decoder.decode_object)
+if isinstance(j_cap_answ, CNNAnswer):
+	img = j_cap_answ.image
+	cv2.imshow("client", img)
+	cv2.waitKey(2000)
+elif isinstance(j_cap_answ, ServiceTask):
+	srv_err: ServiceTask = j_cap_answ
+	print(srv_err.command)
+
+kill_task = ServiceTask("kill")
+j_str = json.dumps(kill_task, cls=json_coder.coder.CustomEncoder)
+socket.send(bytes(j_str, 'utf-8'))
