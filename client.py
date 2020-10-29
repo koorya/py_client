c5fe7947e016d85ad13f189a2b393c72d81ff5bb
Kirill Ivanov <puaktopblm@yandex.ru>
2020-10-29 18:05:37 +0700

    теперь ожидает ответа после посылки завершения процесса Э

--- client.py
+++ client.py
@@ -17,6 +17,7 @@ j_str = json.dumps(kill_task, cls=json_coder.coder.CustomEncoder)
 socket.send(bytes(j_str, 'utf-8'))
 capture_answer_str = socket.recv().decode('utf-8')
 j_cap_answ = json.loads(capture_answer_str, object_hook=json_coder.decoder.decode_object)
+print(j_cap_answ)
 if isinstance(j_cap_answ, CNNAnswer):
 	img = j_cap_answ.image
 	cv2.imshow("client", img)
@@ -28,3 +29,15 @@ elif isinstance(j_cap_answ, ServiceTask):
 kill_task = ServiceTask("kill")
 j_str = json.dumps(kill_task, cls=json_coder.coder.CustomEncoder)
 socket.send(bytes(j_str, 'utf-8'))
+
+print("wait for last resp")
+capture_answer_str = socket.recv().decode('utf-8')
+j_cap_answ = json.loads(capture_answer_str, object_hook=json_coder.decoder.decode_object)
+print(j_cap_answ)
+if isinstance(j_cap_answ, CNNAnswer):
+	img = j_cap_answ.image
+	cv2.imshow("client", img)
+	cv2.waitKey(2000)
+elif isinstance(j_cap_answ, ServiceTask):
+	srv_err: ServiceTask = j_cap_answ
+	print(srv_err.command)
