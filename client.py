7be47b8a467afd700236947ffa64a7a1d1592539
Kirill Ivanov <puaktopblm@yandex.ru>
2020-11-20 07:35:16 +0700

    проверка запроса get all plc vars

--- client.py
+++ client.py
@@ -26,6 +26,20 @@ elif isinstance(j_cap_answ, ServiceTask):
 	srv_err: ServiceTask = j_cap_answ
 	print(srv_err.command)
 
+kill_task = ServiceTask("get all plc vars")
+j_str = json.dumps(kill_task, cls=CustomEncoder)
+socket.send(bytes(j_str, 'utf-8'))
+capture_answer_str = socket.recv().decode('utf-8')
+j_cap_answ = json.loads(capture_answer_str, object_hook=decode_object)
+print(j_cap_answ)
+if isinstance(j_cap_answ, CNNAnswer):
+	img = j_cap_answ.image
+	cv2.imshow("client", img)
+	cv2.waitKey(2000)
+elif isinstance(j_cap_answ, ServiceTask):
+	srv_err: ServiceTask = j_cap_answ
+	print(srv_err.command)
+
 kill_task = ServiceTask("kill")
 j_str = json.dumps(kill_task, cls=CustomEncoder)
 socket.send(bytes(j_str, 'utf-8'))
